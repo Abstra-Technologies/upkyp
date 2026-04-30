e4414c14094ecc9e2e8260ae8f9fe96e07b6228c
BryLim-AI <katoshi.bryan@gmail.com>
2026-04-30 22:44:14 +0800

    perf(constant): removed unsed files

--- constant/docTypes.js
+++
@@ -1,9 +0,0 @@
-export const DOCUMENT_TYPES = [
-    { value: "Postal ID", label: "Postal ID" },
-    { value: "National ID", label: "National ID" },
-    { value: "Passport", label: "Passport" },
-    { value: "Driver' License", label: "Driver' License" },
-    { value: "TIN", label: "TIN" },
-    { value: "SSS ID", label: "SSS ID" },
-    { value: "PhilHealth ID", label: "PhilHealth ID" },
-];
--- constant/pointSystem/points.ts
+++
@@ -1,3 +0,0 @@
-export const POINTS = {
-    PROPERTY_VERIFIED: 30,
-};
